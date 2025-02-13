9df8263753d7a0bc77e3323c06bd323e205cf3d1
mrohling13 <malachi.rohling@yahoo.com>
2025-02-13 11:59:34 -0500

    Task 1 - Customer Invoice Calculation

---
+++ cc_7.js
@@ -0,0 +1,11 @@
+// Task 1: Function Declaration
+
+function calculateInvoice(subtotal, taxRate, discount) {
+    const total = (subtotal +(subtotal * taxRate)) - discount;
+    return `Total Invoice: $${total.toFixed(2)}`;
+}
+
+// Test Data for Task 1
+console.log(calculateInvoice(100, 0.08, 5)); // Expected output: "Total Invoice: $103.00" 
+
+console.log(calculateInvoice(500, 0.1,20)); // Expected output: "Total Invoice: $530.00" 
