07afaf45ce09cc3d34ebde8432d9c0380c5ca058
mrohling13 <malachi.rohling@yahoo.com>
2025-02-13 12:29:00 -0500

    Task 3 - Customer Loyalty Discount

--- cc_7.js
+++ cc_7.js
@@ -1,7 +1,7 @@
 // Task 1: Function Declaration
 
-function calculateInvoice(subtotal, taxRate, discount) {
-    const total = (subtotal +(subtotal * taxRate)) - discount;
+function calculateInvoice(subtotal, taxRate, discount) { // Declaring Invoice Function
+    const total = (subtotal +(subtotal * taxRate)) - discount; // Calculates Final Invoice amount
     return `Total Invoice: $${total.toFixed(2)}`;
 }
 
@@ -13,8 +13,8 @@ console.log(calculateInvoice(500, 0.1,20)); // Expected output: "Total Invoice: 
 
 // Task 2: Function Expression
 
-const calculateHourlyWage = function(salary, hoursPerWeek) {
-    const hourlyWage = salary / (hoursPerWeek * 52);
+const calculateHourlyWage = function(salary, hoursPerWeek) { // Declaring a hourly wage
+    const hourlyWage = salary / (hoursPerWeek * 52); // caluclates wage
     return `Hourly Wage: $${hourlyWage.toFixed(2)}`;
 };
 
@@ -22,3 +22,23 @@ const calculateHourlyWage = function(salary, hoursPerWeek) {
 console.log(calculateHourlyWage(52000, 40)); // Expected output: Expected output: "Hourly Wage: $25.00"
 
 console.log(calculateHourlyWage(75000, 35)); // Expected output: Expected output: "Hourly Wage: $41.21"
+
+// Task 3: Arrow Function
+
+const calculateLoyaltyDiscount = (amount, years) => { 
+    let discountRate;
+    if (years >= 5) {
+        discountRate = 0.15; // Calculates 15% discount
+    } else if (years >= 3) {
+        discountRate = 0.10; // Calculates 10% discount
+    } else {
+        discountRate = 0.05; // Calculates 5% discount
+    }
+    const discountedPrice = amount - (amount * discountRate);
+    return `Discounted Price: $${discountedPrice.toFixed(2)}`;
+};
+
+// Test Data for Task 3
+console.log(calculateLoyaltyDiscount(100, 6)); // Expected output: "Discounted Price: $85.00"
+console.log(calculateLoyaltyDiscount(200, 2)); // Expected output: "Discounted Price: $190.00"
+
